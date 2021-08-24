24ac0fdd0ab48f6073b8cb8a07644661a36fac94
devkeita <keita.mywork@gmail.com>
2021-08-24 23:51:11 +0900

    無駄に画面遷移できるようにしてみた

--- App.tsx
+++ App.tsx
@@ -1,102 +1,28 @@
 import React from "react";
-import {
-  Input,
-  IconButton,
-  Checkbox,
-  Text,
-  VStack,
-  HStack,
-  Heading,
-  Icon,
-  Center,
-  NativeBaseProvider
-} from "native-base";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { NativeBaseProvider } from "native-base";
+import Home from "./screens/Home";
+import Detail from './screens/Detail';
 
-export default function () {
-  const instState = [
-    { title: "code", isCompleted: true },
-    { title: "sleep", isCompleted: false },
-    { title: "repeat", isCompleted: false },
-  ];
-  const [list, setList] = React.useState(instState);
-  const [inputValue, setInputValue] = React.useState("");
-  const addItem = (title: string) => {
-    setList([
-      ...list,
-      {
-        title: title,
-        isCompleted: false,
-      },
-    ]);
-  };
-  const handleDelete = (index: number) => {
-    const temp = list.filter((_, itemI) => itemI !== index);
-    setList(temp);
-  };
-  const handleStatusChange = (index: number) => {
-    const temp = list.map((item, itemI) =>
-      itemI !== index
-        ? item
-        : {
-            ...item,
-            isCompleted: !item.isCompleted,
-          }
-    );
-    setList(temp);
-  };
+export type RootStackParamList = {
+  Home: undefined;
+  Detail: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App() {
   return (
     <NativeBaseProvider>
-    <Center flex={1} safeAreaTop={10}>
-    <VStack space={4} flex={1} w="90%">
-      <Heading color="pink.400">Todo App</Heading>
-      <Input
-        variant="filled"
-        InputRightElement={
-          <IconButton
-            icon={<Icon as={FontAwesome5} name="plus" size={4} />}
-           colorScheme="pink"
-            ml={1}
-            onPress={() => {
-              addItem(inputValue);
-              setInputValue("");
-            }}
-            mr={1}/>
-
-        }
-        onChangeText={(v) => setInputValue(v)}
-        value={inputValue}
-        placeholder="Add Item"
-      />
-      <VStack>
-        {list.map((item, itemI) => (
-          <HStack
-            w="100%"
-            justifyContent="space-between"
-            alignItems="center"
-            key={item.title + itemI.toString()}
-          >
-            <Checkbox
-              colorScheme="pink"
-              isChecked={item.isCompleted}
-              onChange={() => handleStatusChange(itemI)}
-              value={item.title}
-              accessibilityLabel="Todoが完了したかのチェックボックス"
-            >
-              <Text mx={2} strikeThrough={item.isCompleted}>
-                {item.title}
-              </Text>
-            </Checkbox>
-            <IconButton
-              colorScheme="pink"
-              icon={<Icon as={FontAwesome5} name="trash" size={5} />}
-              onPress={() => handleDelete(itemI)}
-            />
-          </HStack>
-        ))}
-      </VStack>
-    </VStack>
-    </Center>
-   </NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ title: "Todo" }} />
+          <Stack.Screen name="Detail" component={Detail} options={{ title: "詳細" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
+
+export default App;
---
+++ screens/Home.tsx
@@ -0,0 +1,107 @@
+import React, { FC } from "react";
+import {
+  Input,
+  IconButton,
+  Checkbox,
+  Text,
+  VStack,
+  HStack,
+  Icon,
+  Center,
+  Button,
+} from "native-base";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const Home: FC<Props> = ({ navigation }) => {
+  const instState = [
+    { title: "code", isCompleted: true },
+    { title: "sleep", isCompleted: false },
+    { title: "repeat", isCompleted: false },
+  ];
+  const [list, setList] = React.useState(instState);
+  const [inputValue, setInputValue] = React.useState("");
+  const addItem = (title: string) => {
+    setList([
+      ...list,
+      {
+        title: title,
+        isCompleted: false,
+      },
+    ]);
+  };
+  const handleDelete = (index: number) => {
+    const temp = list.filter((_, itemI) => itemI !== index);
+    setList(temp);
+  };
+  const handleStatusChange = (index: number) => {
+    const temp = list.map((item, itemI) =>
+      itemI !== index
+        ? item
+        : {
+            ...item,
+            isCompleted: !item.isCompleted,
+          }
+    );
+    setList(temp);
+  };
+  return (
+    <>
+      <Center flex={1} top={5}>
+        <VStack space={4} flex={1} w="90%">
+          <Input
+            variant="filled"
+            InputRightElement={
+              <IconButton
+                icon={<Icon as={FontAwesome5} name="plus" size={4} />}
+              colorScheme="pink"
+                ml={1}
+                onPress={() => {
+                  addItem(inputValue);
+                  setInputValue("");
+                }}
+                mr={1}/>
+
+            }
+            onChangeText={(v) => setInputValue(v)}
+            value={inputValue}
+            placeholder="Add Item"
+          />
+          <VStack>
+            {list.map((item, itemI) => (
+              <HStack
+                w="100%"
+                justifyContent="space-between"
+                alignItems="center"
+                key={item.title + itemI.toString()}
+              >
+                <Checkbox
+                  colorScheme="pink"
+                  isChecked={item.isCompleted}
+                  onChange={() => handleStatusChange(itemI)}
+                  value={item.title}
+                  accessibilityLabel="Todoが完了したかのチェックボックス"
+                >
+                  <Text mx={2} strikeThrough={item.isCompleted}>
+                    {item.title}
+                  </Text>
+                </Checkbox>
+                <IconButton
+                  colorScheme="pink"
+                  icon={<Icon as={FontAwesome5} name="trash" size={5} />}
+                  onPress={() => handleDelete(itemI)}
+                />
+              </HStack>
+            ))}
+          </VStack>
+          <Button onPress={() => navigation.navigate('Detail')}>詳細へ</Button>
+        </VStack>
+      </Center>
+   </>
+  );
+}
+
+export default Home
