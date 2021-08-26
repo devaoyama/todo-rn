a181a181f3f5d886961d4ef003c399126d5892aa
devkeita <keita.mywork@gmail.com>
2021-08-26 21:32:45 +0900

    扶養なコードを削除

--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Center, Button, Heading, useTheme, Container } from "native-base";
+import { Center, Button, Heading, Container } from "native-base";
 
 const AuthScreen = () => {
-  const { colors } = useTheme();
-  console.log(colors);
   return (
     <Center flex={1}>
       <Container w="90%" centerContent>
         <Heading bottom={10}>Todo App</Heading>
-        <Button w="100%" bottom={3}>ログイン</Button>
+        <Button w="100%" bottom={3}>
+          ログイン
+        </Button>
         <Button w="100%" variant="outline" colorScheme="light">
           登録
         </Button>
