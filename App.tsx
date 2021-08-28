6ed233fcea95774f00f85dddff539c9dcc33a8b8
devkeita <keita.mywork@gmail.com>
2021-08-28 13:12:19 +0900

    認証されていない場合の画面を作成

--- App.tsx
+++ App.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, Icon, IconButton } from "native-base";
+import { FontAwesome5 } from "@expo/vector-icons";
 import HomeScreen from "./screens/HomeScreen";
-import DetailScreen from './screens/DetailScreen';
+import DetailScreen from "./screens/DetailScreen";
 import firebase from "./utils/firebase";
-import AuthScreen from './screens/AuthScreen';
+import AuthScreen from "./screens/AuthScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from './screens/RegisterScreen';
 
 export type RootStackParamList = {
   Home: undefined;
   Detail: undefined;
+  Auth: undefined;
+  Login: undefined;
+  Register: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -19,21 +25,82 @@ function App() {
   const [user, setUser] = useState<null | undefined | firebase.User>(undefined);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser)
+    firebase.auth().onAuthStateChanged(setUser);
   }, []);
-  
+
   return (
     <NativeBaseProvider>
-      {user ? (
-        <NavigationContainer>
+      <NavigationContainer>
+        {user ? (
           <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Todo" }} />
-            <Stack.Screen name="Detail" component={DetailScreen} options={{ title: "詳細" }} />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ title: "Todo" }}
+            />
+            <Stack.Screen
+              name="Detail"
+              component={DetailScreen}
+              options={{ title: "詳細" }}
+            />
           </Stack.Navigator>
-        </NavigationContainer>
-      ) : (
-        <AuthScreen />
-      )}
+        ) : (
+          <Stack.Navigator
+            initialRouteName="Auth"
+            screenOptions={{ presentation: "modal" }}
+          >
+            <Stack.Screen
+              name="Auth"
+              component={AuthScreen}
+              options={{ title: "Todo", headerShown: false }}
+            />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={({ navigation }) => ({
+                title: "",
+                headerStyle: {
+                  backgroundColor: '#f2f2f2',
+                },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                  <IconButton
+                    _pressed={{
+                      backgroundColor: 'transparent'
+                    }}
+                    top={1}
+                    right={2}
+                    icon={<Icon as={FontAwesome5} name="times" size={8} color="muted.500" />}
+                    onPress={() => navigation.goBack()}
+                  />
+                ),
+              })}
+            />
+            <Stack.Screen
+              name="Register"
+              component={RegisterScreen}
+              options={({ navigation }) => ({
+                title: "",
+                headerStyle: {
+                  backgroundColor: '#f2f2f2',
+                },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                  <IconButton
+                    _pressed={{
+                      backgroundColor: 'transparent'
+                    }}
+                    top={1}
+                    right={2}
+                    icon={<Icon as={FontAwesome5} name="times" size={8} color="muted.500" />}
+                    onPress={() => navigation.goBack()}
+                  />
+                ),
+              })}
+            />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
--- screens/AuthScreen.tsx
+++ screens/AuthScreen.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { FC } from "react";
 import { Center, Button, Heading, Container } from "native-base";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
 
-const AuthScreen = () => {
+type Props = NativeStackScreenProps<RootStackParamList, "Home">;
+
+const AuthScreen: FC<Props> = ({ navigation }) => {
   return (
     <Center flex={1}>
       <Container w="90%" centerContent>
         <Heading bottom={10}>Todo App</Heading>
-        <Button w="100%" bottom={3}>
+        <Button
+          w="100%"
+          bottom={3}
+          onPress={() => navigation.navigate("Login")}
+        >
           ログイン
         </Button>
-        <Button w="100%" variant="outline" colorScheme="light">
-          登録
+        <Button w="100%" variant="outline" colorScheme="light" onPress={() => navigation.navigate("Register")}>
+          アカウント登録
         </Button>
       </Container>
     </Center>
