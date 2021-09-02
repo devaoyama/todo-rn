93a1326fc35e8dbe948e5722c759ac30df22d179
devkeita <keita.mywork@gmail.com>
2021-09-02 21:34:12 +0900

    recoilのkeyを追加

---
+++ states/keys.ts
@@ -0,0 +1,9 @@
+export enum RecoilAtomKeys {
+  TODO_STATE = "todoState",
+  TODOS_STATE = "todosState",
+}
+
+export enum RecoilSelectorKeys {
+  TODO_TODO = "Todo_todo",
+  TODO_TODOS = "Todo_todos",
+}
